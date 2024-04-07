f4882ffe67bc52cf6e1e53ade7f02892856d579d
abhi-mat <67383732+abhi-mat@users.noreply.github.com>
2024-04-07 23:49:31 +0530

    Update DBConnection.js

--- utils/DBConnection.js
+++ utils/DBConnection.js
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     user: 'mydb_owner',
-    password: 'JnCkMP16GgRQ',
+    password: '*******',
     host: 'ep-long-unit-a5wtgnce.us-east-2.aws.neon.tech',
     database: 'mydb',
     ssl: {
